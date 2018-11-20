02f7afe8530fd86dd9a4afd8833fc8ac9020ea19
tomash970 <tpiljek@gmail.com>
2018-11-20 20:33:17 +0100

    Users abld to persist

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Http\Requests\UsersRequest;
+use App\Role;
+use App\User;
+use Illuminate\Http\Request;
 
 class AdminUsersController extends Controller
 {
@@ -15,7 +17,8 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -26,8 +29,8 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-
-        return view('admin.users.create');
+        $roles = Role::lists('name', 'id')->all();
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -36,9 +39,12 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+        //return $request->all();
+        User::create($request->all());
+        return redirect('/admin/users');
+
     }
 
     /**
